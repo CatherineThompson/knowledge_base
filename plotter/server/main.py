c4df7558504481d50cbbe4c4fba51fe7ad6b658f
Catherine Thompson <catherine.thompson@getweave.com>
2022-09-09 14:42:58 -0600

    remove demos

--- plotter/server/main.py
+++ plotter/server/main.py
@@ -1,11 +1,10 @@
 import config
-# import network
+import network
 import stepper 
-# import uasyncio as asyncio
-# from queue import Queue
-# from server import Server
+import uasyncio as asyncio
+from queue import Queue
+from server import Server
 import time
-from machine import PWM, Pin
 
 def decode_cmd(cmd):
     c = cmd.split(',')
@@ -13,7 +12,6 @@ def decode_cmd(cmd):
     right = int(c[1])
     length = int(c[2])
     return  left, right, length
-    
 
 async def processCmdQueue(q, leftStepper, rightStepper):
     while True:
@@ -33,79 +31,24 @@ async def processCmdQueue(q, leftStepper, rightStepper):
         leftStepper.stop()
         rightStepper.stop()
 
-def stepper_demo():
-    leftStepper = stepper.Stepper(config.LEFT_STEP_PIN, config.LEFT_DIR_PIN, config.LEFT_ENABLE_PIN, config.MAX_FREQ, config.STEP_SIZE)
-    rightStepper = stepper.Stepper(config.RIGHT_STEP_PIN, config.RIGHT_DIR_PIN, config.RIGHT_ENABLE_PIN, config.MAX_FREQ, config.STEP_SIZE)
-
-    leftStepper.move()
-    rightStepper.move()
-
-    time.sleep(5)
-
-    leftStepper.stop()
-    rightStepper.stop()
-
-    time.sleep(5)
-
-    leftStepper.move()
-    rightStepper.move()
-
-    time.sleep(5)
-
-    leftStepper.stop()
-    rightStepper.stop()
-
-    
-stepper_demo()
-
-def pwm_demo():
-    pwm1 = PWM(Pin(config.LEFT_STEP_PIN))
-    pwm2 = PWM(Pin(config.RIGHT_STEP_PIN))
-    # 50%
-    pwm1.duty_u16(32768)
-    pwm2.duty_u16(32768)
-
-    pwm1.freq(int(config.MAX_FREQ / config.STEP_SIZE))
-    pwm2.freq(int(config.MAX_FREQ / config.STEP_SIZE))
-
-    time.sleep(5)
-
-    pwm1.deinit()
-    pwm2.deinit()
-
-    time.sleep(5)
-
-    pwm1.duty_u16(32768)
-    pwm2.duty_u16(32768)
-
-    pwm1.freq(int(config.MAX_FREQ / config.STEP_SIZE))
-    pwm2.freq(int(config.MAX_FREQ / config.STEP_SIZE))
-    
-    time.sleep(5)
-
-    pwm1.deinit()
-    pwm2.deinit()
-
-# pwm_demo()
-
-# async def main():
+async def main():
     # connect to wifi access point
-    # sta = network.WLAN(network.STA_IF)
-    # sta.active(True)
-    # sta.connect(config.WIFI_NAME, config.WIFI_PASSWORD)
-    # print(sta.ifconfig())
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    sta.connect(config.WIFI_NAME, config.WIFI_PASSWORD)
+    print(sta.ifconfig())
 
-    # leftStepper = stepper.Stepper(config.LEFT_STEP_PIN, config.LEFT_DIR_PIN, config.STEP_DELAY_MS, True)
-    # rightStepper = stepper.Stepper(config.RIGHT_STEP_PIN, config.RIGHT_DIR_PIN, config.STEP_DELAY_MS)
+    leftStepper = stepper.Stepper(config.LEFT_STEP_PIN, config.LEFT_DIR_PIN, config.STEP_DELAY_MS, )
+    rightStepper = stepper.Stepper(config.RIGHT_STEP_PIN, config.RIGHT_DIR_PIN, config.STEP_DELAY_MS)
 
-    # q = Queue()
+    q = Queue()
 
-    # asyncio.create_task(processCmdQueue(q, leftStepper, rightStepper))
+    asyncio.create_task(processCmdQueue(q, leftStepper, rightStepper))
 
-    # server = Server(q)
-    # await server.run()
+    server = Server(q)
+    await server.run()
 
-# try:
-#     asyncio.run(main())
-# finally:
-#     asyncio.new_event_loop()  # Clear retained state
+try:
+    asyncio.run(main())
+finally:
+    asyncio.new_event_loop()  # Clear retained state
--- plotter/server/stepper.py
+++ plotter/server/stepper.py
@@ -7,12 +7,13 @@ class Stepper:
   def __init__(self, step_pin, dir_pin, enable_pin, step_size):
     self.pwm = PWM(Pin(step_pin))
     self.dir_pin = Pin(dir_pin)
+    self.enable_pin = Pin(enable_pin)
     self.step_size = step_size
 
   def move(self, dir, freq):
     self.dir_pin(dir)    
     self.pwm.duty_u16(32768)
-    self.pwm.freq(int((freq / self.step_size))
+    self.pwm.freq(int(freq / self.step_size))
 
   def stop(self):
     self.pwm.deinit()
